7b402398a066b33f2bc27e1771fb2c2318f402c4
Hamish Peebles <hamishpeebles@gmail.com>
2024-09-05 22:19:21 +0100

    Use `Record` rather than `[key: ...]` syntax

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -896,11 +896,11 @@ impl<K: TS, V: TS, H> TS for HashMap<K, V, H> {
     }
 
     fn name() -> String {
-        format!("{{ [key in {}]?: {} }}", K::name(), V::name())
+        format!("Record<{}, {}>", K::name(), V::name())
     }
 
     fn inline() -> String {
-        format!("{{ [key in {}]?: {} }}", K::inline(), V::inline())
+        format!("Record<{}, {}>", K::name(), V::name())
     }
 
     fn visit_dependencies(v: &mut impl TypeVisitor)
